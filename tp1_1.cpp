48ff07d21f53933ef31e0ff7565df83ae74c2ab9
delgadoagustin <delgadoagustin147@gmail.com>
2022-03-17 19:58:51 -0300

    avance de la parte 3

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,6 +2,14 @@
 #include<stdlib.h>
 #include<string.h>
 int main(int argc, char const *argv[]){
+    int c = 22;
+    int *p;
+    *p=c;
 printf("Hola mundo\n");
+printf("%i\n", *p);
+printf("%p \n", p);
+
+
 system("pause");
+return 0;
 }
